0fea77225900d8aa30f920c63cb45e8652156d6d
guilhermeonrails <30351153+guilhermeonrails@users.noreply.github.com>
2022-01-20 10:08:20 -0300

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/guilhermeonrails/api-go-gin/controllers"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	rotas := gin.Default()
+	return rotas
+}
+
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacoes)
+	req, _ := http.NewRequest("GET", "/gui", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz":"E ai gui, Tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
